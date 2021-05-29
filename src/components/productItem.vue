2b85d3dd95fd9c9f211b665c769b56a7eeab98e1
BekuhAN <33132631+BekuhAN@users.noreply.github.com>
2021-05-30 01:14:09 +0300

    added about page and modal product

--- src/components/productItem.vue
+++ src/components/productItem.vue
@@ -20,11 +20,88 @@
       </div>
     </div>
     <div class="navigate">
-      <div class="navigate__item product_more">
+      <div class="navigate__item product_more" @click="showModal">
         <font-awesome-icon class="icon" icon="eye" />
       </div>
       <div class="navigate__item product_add">
         <font-awesome-icon class="icon" icon="cart-plus" />
+      </div>
+    </div>
+    <div class="product_item__modal" v-if="isModal">
+      <div class="container">
+        <div class="product_item__modal__body row">
+          <font-awesome-icon
+            class="close_icon"
+            @click="showModal"
+            icon="times"
+          />
+          <div class="col-4 product_item__modal__body__image">
+            <figure>
+              <img v-if="product.image" :src="getImgUrl(product.image)" />
+              <img v-else src="../assets/img/no-photo.png" />
+              <div v-if="product.sale" class="sale">- {{ product.sale }}%</div>
+              <div v-if="product.new" class="new">Новинка</div>
+            </figure>
+          </div>
+          <div class="col-8">
+            <div class="product_item__modal__body__name">
+              {{ product.title }}
+            </div>
+            <div class="product_item__modal__body__content">
+              <div class="description">
+                <div class="title">Описание</div>
+                <p v-if="product.description">{{ product.description }}</p>
+                <p v-else>Описание отсутсвует</p>
+              </div>
+              <div class="description">
+                <div class="title">Характеристики</div>
+                <div
+                  class="detail_item"
+                  v-for="(item, index) in product.detail"
+                  :key="index"
+                >
+                  <div class="detail_item__name">{{ item.name }}</div>
+                  <div class="detail_item__value">{{ item.value }}</div>
+                </div>
+              </div>
+            </div>
+            <div class="product_item__modal__body__footer">
+              <div class="prices">
+                <div class="before_sale" :class="{ sale_price: product.sale }">
+                  {{ product.price }}
+                  <font-awesome-icon class="icon" icon="ruble-sign" />
+                </div>
+                <div v-if="product.sale" class="after_sale">
+                  {{
+                    Math.ceil(
+                      product.price - (product.price * product.sale) / 100
+                    )
+                  }}<font-awesome-icon class="icon" icon="ruble-sign" />
+                </div>
+              </div>
+              <div class="number">
+                <button
+                  class="number__minus"
+                  type="button"
+                  onclick="this.nextElementSibling.stepDown(); this.nextElementSibling.onchange();"
+                >
+                  -
+                </button>
+                <input type="number" max="200" min="0" value="1" />
+                <button
+                  class="number__plus"
+                  type="button"
+                  onclick="this.previousElementSibling.stepUp(); this.previousElementSibling.onchange();"
+                >
+                  +
+                </button>
+              </div>
+              <button class="btn add">
+                В корзину <font-awesome-icon class="icon" icon="cart-plus" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -41,6 +118,9 @@ export default {
   methods: {
     getImgUrl(image) {
       return require("../assets/img/" + image);
+    },
+    showModal() {
+      this.isModal = !this.isModal;
     },
   },
 };
@@ -148,6 +228,184 @@ export default {
       &.product_add {
         background: #e52029;
         transition-delay: 0.1s;
+      }
+    }
+  }
+  &__modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(#000, 0.7);
+    z-index: 5;
+    display: flex;
+    align-items: center;
+    &__body {
+      background: #fff;
+      border-radius: 60px;
+      position: relative;
+      padding: 50px;
+      .close_icon {
+        position: absolute;
+        top: -15px;
+        right: -15px;
+        z-index: 6;
+        font-size: 32px;
+        color: #7fd02c;
+      }
+      &__image {
+        figure {
+          display: flex;
+          justify-content: center;
+          overflow: hidden;
+          height: 300px;
+          img {
+            height: 100%;
+          }
+        }
+      }
+      &__name {
+        font-weight: bold;
+        font-size: 18px;
+      }
+      &__content {
+        max-height: 300px;
+        overflow-y: auto;
+        margin: 30px 0;
+        .description {
+          padding-right: 15px;
+          .title {
+            text-transform: uppercase;
+            font-size: 18px;
+            letter-spacing: 2px;
+            border-bottom: 1px solid #ccc;
+            margin-bottom: 15px;
+          }
+          .detail_item {
+            display: flex;
+            justify-content: space-between;
+            position: relative;
+            z-index: 2;
+            &::before {
+              position: absolute;
+              left: 0;
+              top: 50%;
+              transform: translateY(-50%);
+              content: "";
+              width: 100%;
+              border-bottom: 1px dashed #7fd02c;
+              z-index: -1;
+            }
+            &__name,
+            &__value {
+              background: #fff;
+            }
+            &__name {
+              padding-right: 10px;
+            }
+            &__value {
+              padding-left: 10px;
+            }
+          }
+        }
+      }
+      &__footer {
+        display: flex;
+        .prices {
+          display: flex;
+          align-items: center;
+          color: #e52029;
+          font-weight: bold;
+          font-size: 20px;
+          .icon {
+            font-size: 16px;
+          }
+          .sale_price {
+            color: #2c3e50;
+            font-weight: 400;
+            margin-right: 20px;
+            font-size: 14px;
+            text-decoration: line-through;
+            .icon {
+              font-size: 14px;
+            }
+          }
+        }
+        .number {
+          display: flex;
+          position: relative;
+          width: 100px;
+          margin: 0px 50px;
+          input[type="number"] {
+            display: block;
+            height: 32px;
+            font-size: 22px;
+            width: 100%;
+            border: none;
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            text-align: center;
+            -moz-appearance: textfield;
+            -webkit-appearance: textfield;
+            appearance: textfield;
+            &::-webkit-outer-spin-button,
+            &::-webkit-inner-spin-button {
+              display: none;
+            }
+          }
+          &__minus {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            width: 30px;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: none;
+            font-size: 20px;
+            background: #7fd02c;
+            color: #fff;
+            border-radius: 5px;
+            font-weight: bold;
+          }
+          &__plus {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            width: 30px;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: none;
+            font-size: 20px;
+            background: #7fd02c;
+            color: #fff;
+            border-radius: 5px;
+            font-weight: bold;
+          }
+        }
+        .btn.add {
+          background: #e52029;
+          color: #fff;
+          text-transform: uppercase;
+          border: none;
+          height: 32px;
+          padding: 0px 20px;
+          border-radius: 5px;
+          cursor: pointer;
+          &:hover {
+            transform: scale(1.1);
+          }
+          .icon {
+            margin-left: 10px;
+          }
+        }
       }
     }
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,282 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main class="about_page">
+    <section class="about_page__description">
+      <div class="container">
+        <div class="section_title">О нас</div>
+        <p>
+          Наш интернет-магазин для тех, кому дороги время и деньги. Наше главное
+          отличие от аналогичных ресурсов – реализация продуктов питания и
+          других товаров первой необходимости в рамках прямых поставок от
+          производителей.
+        </p>
+
+        <div class="title">Покупайте с выгодой!</div>
+        <p>
+          Наши цены – одно из главных преимуществ интернет-магазина. Они ниже
+          цен большинства аналогичных ресурсов, реализующих продукты крупных
+          торговых сетей или перепродающих купленное на оптовых базах. Наше
+          ценообразование идет по схеме «производитель – покупатель»: в цепочке
+          нет посредников, в нее не заложены логистические затраты на
+          транспортировку товаров в магазины и наценка самих магазинов. Товары
+          сразу от производителей попадают на собственный распредсклад, а далее
+          по заказам - к вам домой. Этим же объясняются свежесть продуктов и их
+          хорошие сроки годности.
+        </p>
+
+        <div class="title">
+          «Приятно удивлять» - девиз и постоянно действующая программа
+          интернет-магазина.
+        </div>
+        <p>
+          Имея прямые контракты с производителями, Домашкино.рф транслирует на
+          сайте акции для покупателей, сообща тестирует новинки и преподносит
+          приятные сюрпризы. (Так всю весну и весь июнь клиенты сервиса получали
+          в подарок бутылочку биойогурта «Биактив» для поддержки иммунитета во
+          время пандемии).
+        </p>
+
+        <p>
+          Мы сознательно берем на себя жесткие обязательства по доставке
+          продуктов четко в день заказа в выбранные клиентом часы, работая без
+          выходных. Особое внимание уделяем безопасности, гарантируем контроль
+          не только сроков годности продуктов, но и герметичности упаковок.
+        </p>
+        <div class="title">
+          Еще одно преимущество сервиса – он местный, свой, понимающий нужды
+          потребителей вашего региона.
+        </div>
+        <p>
+          Мы готовы принимать заказы по телефону, если вам так удобнее, мы можем
+          отвезти покупки в удаленный район с минимальной стоимостью доставки за
+          счет использования собственных автопарка и штата курьеров. А еще мы
+          поддерживаем фермеров, оперативно развозя вам их свежую натуральную
+          продукцию.
+        </p>
+      </div>
+    </section>
+    <section class="comments">
+      <div class="container">
+        <div class="section_title">Отзывы о нас</div>
+        <Carousel :itemCount="3"
+          ><div class="comments__item">
+            <div class="comments__item__wrapper">
+              <font-awesome-icon class="icon" icon="quote-left" />
+              <div class="message">
+                Должен сказать, что я действительно впечатлен своим недавним
+                заказом. Я уже покупал подобные продукты на других сайтах
+                раньше, но на этот раз я выбрал вас из-за вашего большого выбора
+                и цен.
+              </div>
+              <div class="user">
+                <div class="image"><img src="../assets/img/user-1.jpg" /></div>
+                <div class="name">Светлана</div>
+              </div>
+            </div>
+          </div>
+          <div class="comments__item">
+            <div class="comments__item__wrapper">
+              <font-awesome-icon class="icon" icon="quote-left" />
+              <div class="message">
+                Я только что получил свой заказ и был по-настоящему поражен. Он
+                был так хорошо упакован, и вещи замечательные. Я новичок в том,
+                чтобы заказывать еду онлайн, но это был отличный опыт.
+              </div>
+              <div class="user">
+                <div class="image"><img src="../assets/img/user-2.jpg" /></div>
+                <div class="name">Николай</div>
+              </div>
+            </div>
+          </div>
+          <div class="comments__item">
+            <div class="comments__item__wrapper">
+              <font-awesome-icon class="icon" icon="quote-left" />
+              <div class="message">
+                Я получил свой большой заказ сырых веганских продуктов сегодня
+                через UPS и был очень впечатлен упаковкой и свежестью, а также
+                качеством моих блюд.
+              </div>
+              <div class="user">
+                <div class="image"><img src="../assets/img/user-3.jpg" /></div>
+                <div class="name">Валерия</div>
+              </div>
+            </div>
+          </div>
+        </Carousel>
+      </div>
+    </section>
+    <section class="brands">
+      <div class="container">
+        <Carousel :itemCount="2">
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-1.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-2.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-3.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-4.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-5.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-6.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-7.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-8.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-9.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-10.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-11.png" />
+          </div>
+          <div class="col-2 brands__item">
+            <img src="../assets/img/brand-12.png" />
+          </div>
+        </Carousel>
+      </div>
+    </section>
+  </main>
 </template>
+
+<script>
+import Carousel from "../components/carousel";
+export default {
+  components: {
+    Carousel,
+  },
+};
+</script>
+
+<style lang="scss">
+.about_page {
+  &__description {
+    padding: 100px 0;
+    .section_title {
+      text-align: center;
+    }
+    .title {
+      font-size: 20px;
+      font-weight: bold;
+      letter-spacing: 2px;
+    }
+  }
+}
+.comments {
+  background: url("../assets/img/bg-4.jpg") no-repeat;
+  background-size: auto 100%;
+  background-position-x: center;
+  padding: 100px 0;
+  .section_title {
+    text-align: center;
+    margin-bottom: 50px;
+  }
+  .carousel {
+    position: relative;
+    &::before {
+      position: absolute;
+      content: "";
+      display: block;
+      width: 80%;
+      height: 70%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: #fff;
+      opacity: 0.6;
+      border-radius: 60px;
+    }
+  }
+  &__item {
+    flex: 0 0 1110px;
+    padding: 30px 0;
+    z-index: 2;
+    &__wrapper {
+      width: 70%;
+      margin: 0 auto;
+      box-shadow: 0 0 18px 0 rgba(#000, 0.13);
+      background: #ffffff;
+      border-radius: 60px;
+      padding: 70px 80px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .icon {
+        color: #e52029;
+        font-size: 38px;
+        opacity: 0.5;
+      }
+      .message {
+        text-align: center;
+        font-weight: 500;
+        font-style: italic;
+        margin: 30px 0;
+        color: #777;
+      }
+      .user {
+        display: flex;
+        align-items: center;
+        .image {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+          overflow: hidden;
+          img {
+            height: 100%;
+          }
+        }
+        .name {
+          text-transform: uppercase;
+          font-weight: bold;
+          margin-left: 20px;
+        }
+      }
+    }
+  }
+}
+.brands {
+  padding: 100px 0;
+  background: url("../assets/img/bg-2.jpg") no-repeat;
+  background-size: 100% auto;
+  background-position-y: center;
+  position: relative;
+  .carousel__next,
+  .carousel__prev {
+    opacity: 0.4;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  &::before {
+    position: absolute;
+    content: "";
+    background: #aaa;
+    opacity: 0.8;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  &__item {
+    flex: 0 0 185px;
+    overflow: hidden;
+    filter: grayscale(100%);
+    &:hover {
+      filter: grayscale(0);
+    }
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
